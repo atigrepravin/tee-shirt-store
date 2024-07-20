8da4c42e6627c8e74a1c616d5e1877ddea00036a
Pravin Atigre <atigrepravin@gmail.com>
2024-07-20 19:45:34 +0530

    Implement Header

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
+import Cart from "../icons/cart";
 
 function Header() {
   return (
-    <div className="border-b border-gray-200 py-8 px-8">
-      <Link to="/" className="text-3xl font-semibold">
-        Tee Shirt Store
-      </Link>
+    <div className="border-b border-gray-200">
+      <div className="container mx-auto py-8 flex justify-between">
+        <Link to="/" title="Tee Shirt Store" className="text-3xl font-semibold">
+          Tee Shirt Store
+        </Link>
+        <div className="flex items-center">
+          <Link to="/" className="text-lg">
+            Products
+          </Link>
+          <Link to="/cart" className="ml-6">
+            <Cart />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/layout.tsx
+++ src/layout.tsx
@@ -5,12 +5,7 @@ export const Layout = () => {
   return (
     <>
       <Header />
-      <div className="mx-8">
-        <div className="my-8">
-          <Link to="/">Home</Link> | <Link to="/cart">Cart</Link>
-        </div>
-        <Outlet />
-      </div>
+      <Outlet />
     </>
   );
 };

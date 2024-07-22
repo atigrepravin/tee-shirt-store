892c27f74040ea5eda9dbb897eaf0412f82104b1
Pravin Atigre <atigrepravin@gmail.com>
2024-07-22 19:51:10 +0530

    add workable search

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import { ProductList } from "../components/product/product-list";
 import { getAllProducts } from "../apis/products";
+import { Search } from "../components/form/search";
+import { Product } from "../components/product/product-card";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[] | []>([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -13,7 +16,30 @@ const Home = () => {
     fetchProducts();
   }, []);
 
-  return <ProductList products={products} />;
+  const searchProducts = (data: Product[]) => {
+    const searchParams = ["name", "type", "color"];
+
+    return data.filter((product) => {
+      return searchParams.some((param) => {
+        return product[param as keyof Product]
+          .toString()
+          .toLowerCase()
+          .includes(searchQuery.toLocaleLowerCase());
+      });
+    });
+  };
+
+  return (
+    <div>
+      <div className="max-w-md mx-auto mb-8">
+        <Search
+          onChange={(e: any) => setSearchQuery(e.target.value)}
+          value={searchQuery}
+        />
+      </div>
+      <ProductList products={searchProducts(products)} />
+    </div>
+  );
 };
 
 export default Home;
